f93b7c6d8c0417466e78f3f1b3135a53d2eb7710
tiaggy <110980386+tiaggy@users.noreply.github.com>
2026-03-30 14:49:05 +0200

    Initial commit

---
+++ src/main/java/org/psi/psinema/PsinemaApplication.java
@@ -0,0 +1,13 @@
+package org.psi.psinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/psi/psinema/PsinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.psi.psinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
